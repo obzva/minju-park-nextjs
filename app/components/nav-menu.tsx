2cf71547148d5c2cb9cf0ec005c921e3bb38b1dc
obzva <dngyng1000@gmail.com>
2025-11-11 17:12:35 -0800

    fix nav-menu
    now knobs are also clickable

--- app/components/nav-menu.tsx
+++ app/components/nav-menu.tsx
@@ -4,13 +4,12 @@ import clsx from "clsx";
 import styles from "./nav-menu.module.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { CSSProperties } from "react";
+import { CSSProperties, ReactNode } from "react";
 
 type Props = {
   href: string;
   text: string;
   knobColor: "te-blue" | "te-ochre" | "te-orange";
-  isAnchor?: boolean;
 };
 
 const bgColorClassNames = {
@@ -26,65 +25,57 @@ const knobColorValues = {
   "te-orange": "var(--color-te-orange)",
 } as const;
 
-export default function NavMenu({
-  href,
-  text,
-  knobColor,
-  isAnchor = false,
-}: Props) {
+export default function NavMenu({ href, text, knobColor }: Props) {
   const currPath = usePathname();
 
   return (
-    <div
-      className={clsx(
-        "flex items-center w-fit gap-2 px-2 sm:px-4 cursor-pointer relative",
-        styles.wrapper
-      )}
-      style={
-        {
-          "--knob-inner-color": knobColorValues[knobColor],
-          "--knob-inner-bar-color": knobColor === "te-blue" ? "white" : "black",
-        } as CSSProperties
-      }
-      data-current={!isAnchor && currPath === href}
-    >
+    <Link href={href}>
       <div
         className={clsx(
-          "sm:hidden absolute w-2 h-2 rounded-full top-0 left-0",
-          bgColorClassNames[currPath === href ? knobColor : "te-gray"]
+          "flex items-center w-fit gap-2 px-2 sm:px-4 cursor-pointer relative",
+          styles.wrapper
         )}
-      />
-      <div
-        className={clsx(
-          "hidden sm:flex w-6 h-6 rounded-full border justify-center items-center",
-          styles.knob
-        )}
+        style={
+          {
+            "--knob-inner-color": knobColorValues[knobColor],
+            "--knob-inner-bar-color":
+              knobColor === "te-blue" ? "white" : "black",
+          } as CSSProperties
+        }
+        data-current={currPath === href}
       >
         <div
           className={clsx(
-            "w-4 h-4 rounded-full flex justify-center items-center",
-            bgColorClassNames[currPath === href ? knobColor : "te-gray"],
-            styles["knob-inner"]
+            "sm:hidden absolute w-2 h-2 rounded-full top-0 left-0",
+            bgColorClassNames[currPath === href ? knobColor : "te-gray"]
+          )}
+        />
+        <div
+          className={clsx(
+            "hidden sm:flex w-6 h-6 rounded-full border justify-center items-center",
+            styles.knob
           )}
         >
           <div
             className={clsx(
-              "w-0.5 h-2.5 rounded-xs opacity-25",
-              currPath === href && knobColor === "te-blue"
-                ? "bg-white"
-                : "bg-black",
-                styles["knob-inner-bar"]
+              "w-4 h-4 rounded-full flex justify-center items-center",
+              bgColorClassNames[currPath === href ? knobColor : "te-gray"],
+              styles["knob-inner"]
             )}
-          />
+          >
+            <div
+              className={clsx(
+                "w-0.5 h-2.5 rounded-xs opacity-25",
+                currPath === href && knobColor === "te-blue"
+                  ? "bg-white"
+                  : "bg-black",
+                styles["knob-inner-bar"]
+              )}
+            />
+          </div>
         </div>
+        {text}
       </div>
-      {isAnchor ? (
-        <a href={href} target="_blank" rel="noreferrer">
-          {text}
-        </a>
-      ) : (
-        <Link href={href}>{text}</Link>
-      )}
-    </div>
+    </Link>
   );
 }
